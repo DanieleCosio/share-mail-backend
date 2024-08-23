79d1610ee1afe52116449527f82a74dfc72301a2
daniele.cosio <daniele.cosio99@gmail.com>
2024-08-23 19:25:30 +0200

    feat: loader component

--- internal/app/components/app.ts
+++ internal/app/components/app.ts
@@ -1,6 +1,8 @@
 import { demoClickCallback } from "./templates/demo/demo";
 import { MouseEventCallback } from "./types";
 import Btn from "./atoms/btn/Btn";
+import Icon from "./atoms/icon/Icon";
+import Loader from "./atoms/loader/Loader";
 
 /* GLOBAL FUNCTIONS */
 declare global {
@@ -13,3 +15,5 @@ window.demoClickCallback = demoClickCallback;
 
 /* COMPONENTS */
 customElements.define(Btn.name, Btn);
+customElements.define(Icon.name, Icon);
+customElements.define(Loader.name, Loader);
