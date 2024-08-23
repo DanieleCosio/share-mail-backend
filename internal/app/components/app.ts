ec42aa886c66580533110061089f63dddb625da3
daniele.cosio <daniele.cosio99@gmail.com>
2024-08-23 20:24:12 +0200

    feat: modal component

--- internal/app/components/app.ts
+++ internal/app/components/app.ts
@@ -2,6 +2,7 @@ import { demoClickCallback } from "./templates/demo/demo";
 import { MouseEventCallback } from "./types";
 import Btn from "./atoms/btn/Btn";
 import Icon from "./atoms/icon/Icon";
+import Modal from "./molecules/modal/Modal";
 import Loader from "./atoms/loader/Loader";
 
 /* GLOBAL FUNCTIONS */
@@ -16,4 +17,5 @@ window.demoClickCallback = demoClickCallback;
 /* COMPONENTS */
 customElements.define(Btn.name, Btn);
 customElements.define(Icon.name, Icon);
+customElements.define(Modal.name, Modal);
 customElements.define(Loader.name, Loader);
